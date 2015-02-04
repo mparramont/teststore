df368d6c724d8258776f659586fedb108733bf38
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-02-04 18:42:59 -0200

    Cart Test not passing

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,10 +1,11 @@
 require "test_helper"
+
 class CartTest < MiniTest::Test
     def test_adds_one_item
         cart = Cart.new
         cart.add_item 1
         
-          assert_equal  cart.empty?, false
+        assert_equal  cart.empty?, false
      end
 end
 
