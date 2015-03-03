ec1eca4d89bcbc797fc6cdb2e822fbc16855c5ce
Cassio Kawakita <cassio.kawakita@gmail.com>
2015-03-03 19:27:11 -0300

    Modified Account settings

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
   before_filter :initialize_cart
+  before_action :authenticate_user!
 
   def index
     @orders = Order.order(created_at: :desc).all
